6c55f22a61d33770c5a4e4ae4ba0ce9683d060f3
Victor Prata <victor.prata@buser.com.br>
2022-08-12 13:34:19 -0300

    s02ex09 FUNCIONANDO DE MANEIRA ERRADA

--- lista-02/js09-seis-por-dois.js
+++ lista-02/js09-seis-por-dois.js
@@ -5,10 +5,24 @@
  * trabalhando ou de folga. Difícil fazer planos.
  * Exemplo, como saber no dia 12 de Outubro ele vai estar trabalhando ou de folga?
  * Como fazer um programa que liste os próximos 3 meses com dias de trabalho/folga?
- * 
+ *
  */
 
 // Entrada
 // Pŕoxima volta
 
 // Saída com base na próxima folga, calcular dia de trabalho/folga
+
+function adicionaDias(data_inicial, n_dias) {
+  var resultado = new Date(data_inicial);
+  resultado.setDate(resultado.getDate() + n_dias);
+  return resultado;
+}
+
+var lista_folga = [];
+
+for (let i = 1; i <= 90; i++) {
+  if (i % 7 == 0 || i % 8 == 0) {
+    console.log(adicionaDias("2022-08-12", i));
+  }
+}
