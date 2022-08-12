4921dc373eb1cfe3bdb394b9f453c513783d0a86
Victor Prata <victor.prata@buser.com.br>
2022-08-12 09:40:19 -0300

    s02ex03 ok

--- lista-02/js01-impar-par.js
+++ lista-02/js01-impar-par.js
@@ -1,17 +1,24 @@
 /**
  * Faça um programa que percorra uma lista e gere um outra lista com o
- * mesmo numero de elementos no formato: 'impar' ou 'par' 
- * 
+ * mesmo numero de elementos no formato: 'impar' ou 'par'
+ *
  * ENTRADA          SAÍDA
  * [1,3,5,7,8]      ['impar', 'impar', 'impar', 'impar', 'par']
  */
 
 // Entrada
-var entrada = [1,3,5,7,8]
+var entrada = [1, 3, 5, 7, 8];
 
-var saida = []
+var saida = [];
 
 // gerar a lista de saida
-saida = ['impar', 'impar', 'impar', 'impar', 'par']
 
-console.log(saida)
+entrada.forEach(function (elemento) {
+  if (elemento % 2 == 0) {
+    saida.push("par");
+  } else {
+    saida.push("ímpar");
+  }
+});
+
+console.log(saida);
